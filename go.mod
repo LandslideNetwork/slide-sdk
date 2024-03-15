549a9cf9306b2c1ff9abe16653f79b1b802309ed
n0cte <ilnur.a.g@gmail.com>
2024-03-15 16:13:42 +0400

    update deps

--- go.mod
+++ go.mod
@@ -3,21 +3,20 @@ module github.com/consideritdone/landslidevm
 go 1.22.0
 
 require (
-	github.com/ava-labs/avalanchego v1.11.0
-	github.com/cometbft/cometbft v0.38.5
+	github.com/cometbft/cometbft v0.38.6
+	google.golang.org/grpc v1.60.0
+	google.golang.org/protobuf v1.32.0
 )
 
 require (
-	github.com/DataDog/zstd v1.5.2 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/btcsuite/btcd/btcec/v2 v2.3.2 // indirect
-	github.com/cenkalti/backoff/v4 v4.1.3 // indirect
 	github.com/cespare/xxhash v1.1.0 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/cometbft/cometbft-db v0.7.0 // indirect
 	github.com/cosmos/gogoproto v1.4.11 // indirect
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
-	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.1.0 // indirect
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.0.1 // indirect
 	github.com/dgraph-io/badger/v2 v2.2007.4 // indirect
 	github.com/dgraph-io/ristretto v0.1.1 // indirect
 	github.com/dgryski/go-farm v0.0.0-20200201041132-a6ae2369ad13 // indirect
@@ -25,23 +24,14 @@ require (
 	github.com/go-kit/kit v0.12.0 // indirect
 	github.com/go-kit/log v0.2.1 // indirect
 	github.com/go-logfmt/logfmt v0.6.0 // indirect
-	github.com/go-logr/logr v1.3.0 // indirect
-	github.com/go-logr/stdr v1.2.2 // indirect
-	github.com/go-ole/go-ole v1.2.6 // indirect
 	github.com/golang/glog v1.1.2 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
-	github.com/golang/snappy v0.0.5-0.20220116011046-fa5810519dcb // indirect
+	github.com/golang/snappy v0.0.4 // indirect
 	github.com/google/btree v1.1.2 // indirect
 	github.com/google/go-cmp v0.6.0 // indirect
-	github.com/google/renameio/v2 v2.0.0 // indirect
-	github.com/gorilla/rpc v1.2.0 // indirect
-	github.com/grpc-ecosystem/go-grpc-prometheus v1.2.0 // indirect
-	github.com/grpc-ecosystem/grpc-gateway/v2 v2.12.0 // indirect
 	github.com/jmhodges/levigo v1.0.0 // indirect
 	github.com/klauspost/compress v1.17.0 // indirect
 	github.com/matttproud/golang_protobuf_extensions v1.0.4 // indirect
-	github.com/mr-tron/base58 v1.2.0 // indirect
-	github.com/nbutton23/zxcvbn-go v0.0.0-20210217022336-fa2cb2858354 // indirect
 	github.com/oasisprotocol/curve25519-voi v0.0.0-20220708102147-0a8a51822cae // indirect
 	github.com/petermattis/goid v0.0.0-20180202154549-b0b1615b78e5 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
@@ -51,45 +41,16 @@ require (
 	github.com/prometheus/common v0.42.0 // indirect
 	github.com/prometheus/procfs v0.12.0 // indirect
 	github.com/sasha-s/go-deadlock v0.3.1 // indirect
-	github.com/shirou/gopsutil v3.21.11+incompatible // indirect
 	github.com/stretchr/testify v1.8.4 // indirect
-	github.com/supranational/blst v0.3.11 // indirect
-	github.com/syndtr/goleveldb v1.0.1-0.20220614013038-64ee5596c38a // indirect
+	github.com/syndtr/goleveldb v1.0.1-0.20210819022825-2ae1ddf74ef7 // indirect
 	github.com/tecbot/gorocksdb v0.0.0-20191217155057-f0fad39f321c // indirect
-	github.com/tklauser/go-sysconf v0.3.5 // indirect
-	github.com/tklauser/numcpus v0.2.2 // indirect
-	github.com/yusufpapurcu/wmi v1.2.2 // indirect
 	go.etcd.io/bbolt v1.3.6 // indirect
-	go.opentelemetry.io/otel v1.14.0 // indirect
-	go.opentelemetry.io/otel/exporters/otlp/internal/retry v1.11.0 // indirect
-	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.11.0 // indirect
-	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc v1.11.0 // indirect
-	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp v1.11.0 // indirect
-	go.opentelemetry.io/otel/sdk v1.14.0 // indirect
-	go.opentelemetry.io/otel/trace v1.14.0 // indirect
-	go.opentelemetry.io/proto/otlp v0.19.0 // indirect
-	go.uber.org/mock v0.4.0 // indirect
-	go.uber.org/multierr v1.10.0 // indirect
-	go.uber.org/zap v1.26.0 // indirect
 	golang.org/x/crypto v0.18.0 // indirect
-	golang.org/x/exp v0.0.0-20231127185646-65229373498e // indirect
+	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
 	golang.org/x/net v0.20.0 // indirect
-	golang.org/x/sync v0.5.0 // indirect
 	golang.org/x/sys v0.16.0 // indirect
-	golang.org/x/term v0.16.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
-	gonum.org/v1/gonum v0.12.0 // indirect
-	google.golang.org/genproto v0.0.0-20231106174013-bbf56f31fb17 // indirect
-	google.golang.org/genproto/googleapis/api v0.0.0-20231030173426-d783a09b4405 // indirect
+	golang.org/x/xerrors v0.0.0-20220907171357-04be3eba64a2 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20231120223509-83a465c0220f // indirect
-	google.golang.org/grpc v1.60.0 // indirect
-	google.golang.org/protobuf v1.31.0 // indirect
-	gopkg.in/natefinch/lumberjack.v2 v2.0.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
-)
-
-replace (
-	go.opentelemetry.io/otel => go.opentelemetry.io/otel v1.11.0
-	go.opentelemetry.io/otel/sdk => go.opentelemetry.io/otel/sdk v1.11.0 // indirect
-	go.opentelemetry.io/otel/trace => go.opentelemetry.io/otel/trace v1.11.0 // indirect
 )
--- landslidevm.go
+++ landslidevm.go
@@ -2,39 +2,14 @@ package landslidevm
 
 import (
 	"context"
-	"net/http"
-	"time"
 
-	"github.com/ava-labs/avalanchego/api/health"
-	"github.com/ava-labs/avalanchego/database"
-	"github.com/ava-labs/avalanchego/ids"
-	"github.com/ava-labs/avalanchego/snow"
-	"github.com/ava-labs/avalanchego/snow/consensus/snowman"
-	"github.com/ava-labs/avalanchego/snow/engine/common"
-	"github.com/ava-labs/avalanchego/snow/engine/snowman/block"
-	"github.com/ava-labs/avalanchego/snow/validators"
-	"github.com/ava-labs/avalanchego/version"
-	"github.com/ava-labs/avalanchego/vms/rpcchainvm"
-	"github.com/ava-labs/avalanchego/vms/rpcchainvm/grpcutils"
 	"github.com/cometbft/cometbft/abci/types"
 	"github.com/cometbft/cometbft/proxy"
+	emptypb "google.golang.org/protobuf/types/known/emptypb"
 )
 
 var (
-	_ common.AppRequestHandler            = (*LandslideVM)(nil)
-	_ common.AppResponseHandler           = (*LandslideVM)(nil)
-	_ common.AppGossipHandler             = (*LandslideVM)(nil)
-	_ common.NetworkAppHandler            = (*LandslideVM)(nil)
-	_ common.CrossChainAppRequestHandler  = (*LandslideVM)(nil)
-	_ common.CrossChainAppResponseHandler = (*LandslideVM)(nil)
-	_ common.CrossChainAppHandler         = (*LandslideVM)(nil)
-	_ common.AppHandler                   = (*LandslideVM)(nil)
-	_ health.Checker                      = (*LandslideVM)(nil)
-	_ validators.Connector                = (*LandslideVM)(nil)
-	_ common.VM                           = (*LandslideVM)(nil)
-	_ block.Getter                        = (*LandslideVM)(nil)
-	_ block.Parser                        = (*LandslideVM)(nil)
-	_ block.ChainVM                       = (*LandslideVM)(nil)
+	_ LandslideVMServer = (*LandslideVM)(nil)
 )
 
 type (
@@ -62,284 +37,163 @@ func New(creator AppCreator) *LandslideVM {
 	return &LandslideVM{appCreator: creator}
 }
 
-func Serve(ctx context.Context, creator AppCreator, opts ...grpcutils.ServerOption) {
-	rpcchainvm.Serve(ctx, New(creator), opts...)
-}
-
-// Notify this engine of a request for an AppResponse with the same
-// requestID.
-//
-// The meaning of request, and what should be sent in response to it, is
-// application (VM) specific.
-//
-// It is not guaranteed that request is well-formed or valid.
-//
-// This function can be called by any node at any time.
-func (vm *LandslideVM) AppRequest(
-	ctx context.Context,
-	nodeID ids.NodeID,
-	requestID uint32,
-	deadline time.Time,
-	request []byte,
-) error {
-	panic("ToDo: implement me")
-}
-
-// Notify this engine of the response to a previously sent AppRequest with
-// the same requestID.
-//
-// The meaning of response is application (VM) specifc.
-//
-// It is not guaranteed that response is well-formed or valid.
-func (vm *LandslideVM) AppResponse(
-	ctx context.Context,
-	nodeID ids.NodeID,
-	requestID uint32,
-	response []byte,
-) error {
-	panic("ToDo: implement me")
-}
-
-// Notify this engine that an AppRequest it issued has failed.
-//
-// This function will be called if an AppRequest message with nodeID and
-// requestID was previously sent by this engine and will not receive a
-// response.
-func (vm *LandslideVM) AppRequestFailed(
-	ctx context.Context,
-	nodeID ids.NodeID,
-	requestID uint32,
-	appErr *common.AppError,
-) error {
-	panic("ToDo: implement me")
-}
-
-// Notify this engine of a gossip message from nodeID.
-//
-// The meaning of msg is application (VM) specific, and the VM defines how
-// to react to this message.
-//
-// This message is not expected in response to any event, and it does not
-// need to be responded to.
-func (vm *LandslideVM) AppGossip(
-	ctx context.Context,
-	nodeID ids.NodeID,
-	msg []byte,
-) error {
-	panic("ToDo: implement me")
-}
-
-// Notify this engine of a request for a CrossChainAppResponse with the same
-// requestID.
-//
-// The meaning of request, and what should be sent in response to it, is
-// application (VM) specific.
-//
-// Guarantees surrounding the request are specific to the implementation of
-// the requesting VM. For example, the request may or may not be guaranteed
-// to be well-formed/valid depending on the implementation of the requesting
-// VM.
-func (vm *LandslideVM) CrossChainAppRequest(
-	ctx context.Context,
-	chainID ids.ID,
-	requestID uint32,
-	deadline time.Time,
-	request []byte,
-) error {
-	panic("ToDo: implement me")
-}
-
-// Notify this engine of the response to a previously sent
-// CrossChainAppRequest with the same requestID.
-//
-// The meaning of response is application (VM) specifc.
-//
-// Guarantees surrounding the response are specific to the implementation of
-// the responding VM. For example, the response may or may not be guaranteed
-// to be well-formed/valid depending on the implementation of the requesting
-// VM.
-func (vm *LandslideVM) CrossChainAppResponse(
-	ctx context.Context,
-	chainID ids.ID,
-	requestID uint32,
-	response []byte,
-) error {
-	panic("ToDo: implement me")
-}
-
-// Notify this engine that a CrossChainAppRequest it issued has failed.
-//
-// This function will be called if a CrossChainAppRequest message with
-// nodeID and requestID was previously sent by this engine and will not
-// receive a response.
-func (vm *LandslideVM) CrossChainAppRequestFailed(
-	ctx context.Context,
-	chainID ids.ID,
-	requestID uint32,
-	appErr *common.AppError,
-) error {
-	panic("ToDo: implement me")
-}
-
-// HealthCheck returns health check results and, if not healthy, a non-nil
-// error
-//
-// It is expected that the results are json marshallable.
-func (vm *LandslideVM) HealthCheck(context.Context) (interface{}, error) {
-	panic("ToDo: implement me")
-}
-
-// Connector represents a handler that is called when a connection is marked as
-// connected or disconnected
-func (vm *LandslideVM) Connected(
-	ctx context.Context,
-	nodeID ids.NodeID,
-	nodeVersion *version.Application,
-) error {
-	panic("ToDo: implement me")
-}
-
-// Connector represents a handler that is called when a connection is marked as
-// connected or disconnected
-func (vm *LandslideVM) Disconnected(ctx context.Context, nodeID ids.NodeID) error {
-	panic("ToDo: implement me")
-}
-
 // Initialize this VM.
-// [chainCtx]: Metadata about this VM.
-//
-//	[chainCtx.networkID]: The ID of the network this VM's chain is
-//	                      running on.
-//	[chainCtx.chainID]: The unique ID of the chain this VM is running on.
-//	[chainCtx.Log]: Used to log messages
-//	[chainCtx.NodeID]: The unique staker ID of this node.
-//	[chainCtx.Lock]: A Read/Write lock shared by this VM and the
-//	                 consensus engine that manages this VM. The write
-//	                 lock is held whenever code in the consensus engine
-//	                 calls the VM.
-//
-// [dbManager]: The manager of the database this VM will persist data to.
-// [genesisBytes]: The byte-encoding of the genesis information of this
-//
-//	VM. The VM uses it to initialize its state. For
-//	example, if this VM were an account-based payments
-//	system, `genesisBytes` would probably contain a genesis
-//	transaction that gives coins to some accounts, and this
-//	transaction would be in the genesis block.
-//
-// [toEngine]: The channel used to send messages to the consensus engine.
-// [fxs]: Feature extensions that attach to this VM.
-func (vm *LandslideVM) Initialize(
-	ctx context.Context,
-	chainCtx *snow.Context,
-	db database.Database,
-	genesisBytes []byte,
-	upgradeBytes []byte,
-	configBytes []byte,
-	toEngine chan<- common.Message,
-	fxs []*common.Fx,
-	appSender common.AppSender,
-) error {
-
+func (vm *LandslideVM) Initialize(context.Context, *InitializeRequest) (*InitializeResponse, error) {
 	panic("ToDo: implement me")
 }
 
 // SetState communicates to VM its next state it starts
-func (vm *LandslideVM) SetState(ctx context.Context, state snow.State) error {
+func (vm *LandslideVM) SetState(context.Context, *SetStateRequest) (*SetStateResponse, error) {
 	panic("ToDo: implement me")
 }
 
 // Shutdown is called when the node is shutting down.
-func (vm *LandslideVM) Shutdown(context.Context) error {
-	panic("ToDo: implement me")
-}
-
-// Version returns the version of the VM.
-func (vm *LandslideVM) Version(context.Context) (string, error) {
+func (vm *LandslideVM) Shutdown(context.Context, *emptypb.Empty) (*emptypb.Empty, error) {
 	panic("ToDo: implement me")
 }
 
 // Creates the HTTP handlers for custom chain network calls.
-//
-// This exposes handlers that the outside world can use to communicate with
-// the chain. Each handler has the path:
-// [Address of node]/ext/bc/[chain ID]/[extension]
-//
-// Returns a mapping from [extension]s to HTTP handlers.
-//
-// For example, if this VM implements an account-based payments system,
-// it have an extension called `accounts`, where clients could get
-// information about their accounts.
-func (vm *LandslideVM) CreateHandlers(context.Context) (map[string]http.Handler, error) {
+func (vm *LandslideVM) CreateHandlers(context.Context, *emptypb.Empty) (*CreateHandlersResponse, error) {
 	panic("ToDo: implement me")
 }
 
-// Attempt to load a block.
-//
-// If the block does not exist, database.ErrNotFound should be returned.
-//
-// It is expected that blocks that have been successfully verified should be
-// returned correctly. It is also expected that blocks that have been
-// accepted by the consensus engine should be able to be fetched. It is not
-// required for blocks that have been rejected by the consensus engine to be
-// able to be fetched.
-func (vm *LandslideVM) GetBlock(ctx context.Context, blkID ids.ID) (snowman.Block, error) {
+func (vm *LandslideVM) Connected(context.Context, *ConnectedRequest) (*emptypb.Empty, error) {
 	panic("ToDo: implement me")
 }
 
-// Attempt to create a block from a stream of bytes.
-//
-// The block should be represented by the full byte array, without extra
-// bytes.
-//
-// It is expected for all historical blocks to be parseable.
-func (vm *LandslideVM) ParseBlock(ctx context.Context, blockBytes []byte) (snowman.Block, error) {
+func (vm *LandslideVM) Disconnected(context.Context, *DisconnectedRequest) (*emptypb.Empty, error) {
 	panic("ToDo: implement me")
 }
 
 // Attempt to create a new block from data contained in the VM.
-//
-// If the VM doesn't want to issue a new block, an error should be
-// returned.
-func (vm *LandslideVM) BuildBlock(context.Context) (snowman.Block, error) {
+func (vm *LandslideVM) BuildBlock(context.Context, *BuildBlockRequest) (*BuildBlockResponse, error) {
+	panic("ToDo: implement me")
+}
+
+// Attempt to create a block from a stream of bytes.
+func (vm *LandslideVM) ParseBlock(context.Context, *ParseBlockRequest) (*ParseBlockResponse, error) {
+	panic("ToDo: implement me")
+}
+
+// Attempt to load a block.
+func (vm *LandslideVM) GetBlock(context.Context, *GetBlockRequest) (*GetBlockResponse, error) {
 	panic("ToDo: implement me")
 }
 
 // Notify the VM of the currently preferred block.
-//
-// This should always be a block that has no children known to consensus.
-func (vm *LandslideVM) SetPreference(ctx context.Context, blkID ids.ID) error {
+func (vm *LandslideVM) SetPreference(context.Context, *SetPreferenceRequest) (*emptypb.Empty, error) {
 	panic("ToDo: implement me")
 }
 
-// LastAccepted returns the ID of the last accepted block.
-//
-// If no blocks have been accepted by consensus yet, it is assumed there is
-// a definitionally accepted block, the Genesis block, that will be
-// returned.
-func (vm *LandslideVM) LastAccepted(context.Context) (ids.ID, error) {
+// Attempt to verify the health of the VM.
+func (vm *LandslideVM) Health(context.Context, *emptypb.Empty) (*HealthResponse, error) {
 	panic("ToDo: implement me")
 }
 
-// VerifyHeightIndex should return:
-//   - nil if the height index is available.
-//   - ErrIndexIncomplete if the height index is not currently available.
-//   - Any other non-standard error that may have occurred when verifying the
-//     index.
-//
-// TODO: Remove after v1.11.x activates.
-func (vm *LandslideVM) VerifyHeightIndex(context.Context) error {
+// Version returns the version of the VM.
+func (vm *LandslideVM) Version(context.Context, *emptypb.Empty) (*VersionResponse, error) {
 	panic("ToDo: implement me")
 }
 
-// GetBlockIDAtHeight returns:
-// - The ID of the block that was accepted with [height].
-// - database.ErrNotFound if the [height] index is unknown.
+// Notify this engine of a request for data from [nodeID].
+func (vm *LandslideVM) AppRequest(context.Context, *AppRequestMsg) (*emptypb.Empty, error) {
+	panic("ToDo: implement me")
+}
+
+// Notify this engine that an AppRequest message it sent to [nodeID] with
+// request ID [requestID] failed.
+func (vm *LandslideVM) AppRequestFailed(context.Context, *AppRequestFailedMsg) (*emptypb.Empty, error) {
+	panic("ToDo: implement me")
+}
+
+// Notify this engine of a response to the AppRequest message it sent to
+// [nodeID] with request ID [requestID].
+func (vm *LandslideVM) AppResponse(context.Context, *AppResponseMsg) (*emptypb.Empty, error) {
+	panic("ToDo: implement me")
+}
+
+// Notify this engine of a gossip message from [nodeID].
+func (vm *LandslideVM) AppGossip(context.Context, *AppGossipMsg) (*emptypb.Empty, error) {
+	panic("ToDo: implement me")
+}
+
+// Attempts to gather metrics from a VM.
+func (vm *LandslideVM) Gather(context.Context, *emptypb.Empty) (*GatherResponse, error) {
+	panic("ToDo: implement me")
+}
+
+func (vm *LandslideVM) CrossChainAppRequest(context.Context, *CrossChainAppRequestMsg) (*emptypb.Empty, error) {
+	panic("ToDo: implement me")
+}
+
+func (vm *LandslideVM) CrossChainAppRequestFailed(context.Context, *CrossChainAppRequestFailedMsg) (*emptypb.Empty, error) {
+	panic("ToDo: implement me")
+}
+
+func (vm *LandslideVM) CrossChainAppResponse(context.Context, *CrossChainAppResponseMsg) (*emptypb.Empty, error) {
+	panic("ToDo: implement me")
+}
+
+// BatchedChainVM
+func (vm *LandslideVM) GetAncestors(context.Context, *GetAncestorsRequest) (*GetAncestorsResponse, error) {
+	panic("ToDo: implement me")
+}
+
+func (vm *LandslideVM) BatchedParseBlock(context.Context, *BatchedParseBlockRequest) (*BatchedParseBlockResponse, error) {
+	panic("ToDo: implement me")
+}
+
+// HeightIndexedChainVM
+func (vm *LandslideVM) VerifyHeightIndex(context.Context, *emptypb.Empty) (*VerifyHeightIndexResponse, error) {
+	panic("ToDo: implement me")
+}
+
+func (vm *LandslideVM) GetBlockIDAtHeight(context.Context, *GetBlockIDAtHeightRequest) (*GetBlockIDAtHeightResponse, error) {
+	panic("ToDo: implement me")
+}
+
+// StateSyncableVM
 //
-// Note: A returned value of [database.ErrNotFound] typically means that the
-//
-//	underlying VM was state synced and does not have access to the
-//	blockID at [height].
-func (vm *LandslideVM) GetBlockIDAtHeight(ctx context.Context, height uint64) (ids.ID, error) {
+// StateSyncEnabled indicates whether the state sync is enabled for this VM.
+func (vm *LandslideVM) StateSyncEnabled(context.Context, *emptypb.Empty) (*StateSyncEnabledResponse, error) {
+	panic("ToDo: implement me")
+}
+
+// GetOngoingSyncStateSummary returns an in-progress state summary if it exists.
+func (vm *LandslideVM) GetOngoingSyncStateSummary(context.Context, *emptypb.Empty) (*GetOngoingSyncStateSummaryResponse, error) {
+	panic("ToDo: implement me")
+}
+
+// GetLastStateSummary returns the latest state summary.
+func (vm *LandslideVM) GetLastStateSummary(context.Context, *emptypb.Empty) (*GetLastStateSummaryResponse, error) {
+	panic("ToDo: implement me")
+}
+
+// ParseStateSummary parses a state summary out of [summaryBytes].
+func (vm *LandslideVM) ParseStateSummary(context.Context, *ParseStateSummaryRequest) (*ParseStateSummaryResponse, error) {
+	panic("ToDo: implement me")
+}
+
+// GetStateSummary retrieves the state summary that was generated at height
+// [summaryHeight].
+func (vm *LandslideVM) GetStateSummary(context.Context, *GetStateSummaryRequest) (*GetStateSummaryResponse, error) {
+	panic("ToDo: implement me")
+}
+
+// Block
+func (vm *LandslideVM) BlockVerify(context.Context, *BlockVerifyRequest) (*BlockVerifyResponse, error) {
+	panic("ToDo: implement me")
+}
+
+func (vm *LandslideVM) BlockAccept(context.Context, *BlockAcceptRequest) (*emptypb.Empty, error) {
+	panic("ToDo: implement me")
+}
+
+func (vm *LandslideVM) BlockReject(context.Context, *BlockRejectRequest) (*emptypb.Empty, error) {
+	panic("ToDo: implement me")
+}
+
+// StateSummary
+func (vm *LandslideVM) StateSummaryAccept(context.Context, *StateSummaryAcceptRequest) (*StateSummaryAcceptResponse, error) {
 	panic("ToDo: implement me")
 }
