4cd7bf8d511f15a208ced7436f925e3d987a1223
Ivan Sukach <ivansukach@github.com>
2024-05-03 16:19:53 +0200

    introduce deep equal and expected results for rpc tests

--- vm/rpc_test.go
+++ vm/rpc_test.go
@@ -13,6 +13,10 @@ import (
 	"github.com/consideritdone/landslidevm/jsonrpc"
 )
 
+var (
+	expectedResultHealth = ctypes.ResultHealth{}
+)
+
 func setupRPC(t *testing.T) (*http.Server, *LandslideVM, *client.Client) {
 	vm := newFreshKvApp(t)
 	vmLnd := vm.(*LandslideVM)
@@ -67,37 +71,38 @@ func TestRPC(t *testing.T) {
 		method   string
 		params   map[string]interface{}
 		response interface{}
+		expected interface{}
 	}{
-		{"Health", "health", map[string]interface{}{}, new(ctypes.ResultHealth)},
-		{"Status", "status", map[string]interface{}{}, new(ctypes.ResultStatus)},
-		{"NetInfo", "net_info", map[string]interface{}{}, new(ctypes.ResultNetInfo)},
-		{"Blockchain", "blockchain", map[string]interface{}{}, new(ctypes.ResultBlockchainInfo)},
-		{"Genesis", "genesis", map[string]interface{}{}, new(ctypes.ResultGenesis)},
-		{"GenesisChunk", "genesis_chunked", map[string]interface{}{}, new(ctypes.ResultGenesisChunk)},
-		{"Block", "block", map[string]interface{}{}, new(ctypes.ResultBlock)},
-		{"BlockResults", "block_results", map[string]interface{}{}, new(ctypes.ResultBlockResults)},
-		{"Commit", "commit", map[string]interface{}{}, new(ctypes.ResultCommit)},
-		{"Header", "header", map[string]interface{}{}, new(ctypes.ResultHeader)},
-		{"HeaderByHash", "header_by_hash", map[string]interface{}{}, new(ctypes.ResultHeader)},
-		{"CheckTx", "check_tx", map[string]interface{}{}, new(ctypes.ResultCheckTx)},
-		{"Tx", "tx", map[string]interface{}{}, new(ctypes.ResultTx)},
-		{"TxSearch", "tx_search", map[string]interface{}{}, new(ctypes.ResultTxSearch)},
-		{"BlockSearch", "block_search", map[string]interface{}{}, new(ctypes.ResultBlockSearch)},
-		{"Validators", "validators", map[string]interface{}{}, new(ctypes.ResultValidators)},
-		{"DumpConsensusState", "dump_consensus_state", map[string]interface{}{}, new(ctypes.ResultDumpConsensusState)},
-		{"ConsensusState", "consensus_state", map[string]interface{}{}, new(ctypes.ResultConsensusState)},
-		{"ConsensusParams", "consensus_params", map[string]interface{}{}, new(ctypes.ResultConsensusParams)},
-		{"UnconfirmedTxs", "unconfirmed_txs", map[string]interface{}{}, new(ctypes.ResultUnconfirmedTxs)},
-		{"NumUnconfirmedTxs", "num_unconfirmed_txs", map[string]interface{}{}, new(ctypes.ResultUnconfirmedTxs)},
-		{"BroadcastTxSync", "broadcast_tx_sync", map[string]interface{}{}, new(ctypes.ResultBroadcastTx)},
-		{"BroadcastTxAsync", "broadcast_tx_async", map[string]interface{}{}, new(ctypes.ResultBroadcastTx)},
+		{"Health", "health", map[string]interface{}{}, new(ctypes.ResultHealth), ctypes.ResultHealth{}},
+		{"Status", "status", map[string]interface{}{}, new(ctypes.ResultStatus), ctypes.ResultStatus{}},
+		{"NetInfo", "net_info", map[string]interface{}{}, new(ctypes.ResultNetInfo), ctypes.ResultNetInfo{}},
+		{"Blockchain", "blockchain", map[string]interface{}{}, new(ctypes.ResultBlockchainInfo), ctypes.ResultBlockchainInfo{}},
+		{"Genesis", "genesis", map[string]interface{}{}, new(ctypes.ResultGenesis), ctypes.ResultGenesis{}},
+		{"GenesisChunk", "genesis_chunked", map[string]interface{}{}, new(ctypes.ResultGenesisChunk), ctypes.ResultGenesisChunk{}},
+		{"Block", "block", map[string]interface{}{}, new(ctypes.ResultBlock), ctypes.ResultBlock{}},
+		{"BlockResults", "block_results", map[string]interface{}{}, new(ctypes.ResultBlockResults), ctypes.ResultBlockResults{}},
+		{"Commit", "commit", map[string]interface{}{}, new(ctypes.ResultCommit), ctypes.ResultCommit{}},
+		{"Header", "header", map[string]interface{}{}, new(ctypes.ResultHeader), ctypes.ResultHeader{}},
+		{"HeaderByHash", "header_by_hash", map[string]interface{}{}, new(ctypes.ResultHeader), ctypes.ResultHeader{}},
+		{"CheckTx", "check_tx", map[string]interface{}{}, new(ctypes.ResultCheckTx), ctypes.ResultCheckTx{}},
+		{"Tx", "tx", map[string]interface{}{}, new(ctypes.ResultTx), ctypes.ResultTx{}},
+		{"TxSearch", "tx_search", map[string]interface{}{}, new(ctypes.ResultTxSearch), ctypes.ResultTxSearch{}},
+		{"BlockSearch", "block_search", map[string]interface{}{}, new(ctypes.ResultBlockSearch), ctypes.ResultBlockSearch{}},
+		{"Validators", "validators", map[string]interface{}{}, new(ctypes.ResultValidators), ctypes.ResultValidators{}},
+		{"DumpConsensusState", "dump_consensus_state", map[string]interface{}{}, new(ctypes.ResultDumpConsensusState), ctypes.ResultDumpConsensusState{}},
+		{"ConsensusState", "consensus_state", map[string]interface{}{}, new(ctypes.ResultConsensusState), ctypes.ResultConsensusState{}},
+		{"ConsensusParams", "consensus_params", map[string]interface{}{}, new(ctypes.ResultConsensusParams), ctypes.ResultConsensusParams{}},
+		{"UnconfirmedTxs", "unconfirmed_txs", map[string]interface{}{}, new(ctypes.ResultUnconfirmedTxs), ctypes.ResultUnconfirmedTxs{}},
+		{"NumUnconfirmedTxs", "num_unconfirmed_txs", map[string]interface{}{}, new(ctypes.ResultUnconfirmedTxs), ctypes.ResultUnconfirmedTxs{}},
+		{"BroadcastTxSync", "broadcast_tx_sync", map[string]interface{}{}, new(ctypes.ResultBroadcastTx), ctypes.ResultBroadcastTx{}},
+		{"BroadcastTxAsync", "broadcast_tx_async", map[string]interface{}{}, new(ctypes.ResultBroadcastTx), ctypes.ResultBroadcastTx{}},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			_, err := client.Call(context.Background(), tt.method, tt.params, tt.response)
 			require.NoError(t, err)
-			t.Logf("%s result %+v", tt.name, tt.response)
+			require.Equal(t, tt.expected, tt.response)
 		})
 	}
 }
