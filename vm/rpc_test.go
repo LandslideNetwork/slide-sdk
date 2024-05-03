5aa6135d0f2c74dc741f852f15977666ea03c154
Vasyl Naumenko <vnau108@gmail.com>
2024-05-03 12:18:19 +0300

    rpc table test

--- vm/rpc_test.go
+++ vm/rpc_test.go
@@ -23,8 +23,8 @@ func setupRPC(t *testing.T) (*http.Server, *LandslideVM, *client.Client) {
 	server := &http.Server{Addr: address, Handler: mux}
 	go func() {
 		server.ListenAndServe()
-		//panic(err)
-		//require.NoError(t, err)
+		// panic(err)
+		// require.NoError(t, err)
 	}()
 
 	// wait for servers to start
@@ -56,4 +56,48 @@ func TestStatus(t *testing.T) {
 	require.NoError(t, err)
 
 	t.Logf("Status result %+v", result)
+}
+
+func TestRPC(t *testing.T) {
+	server, _, client := setupRPC(t)
+	defer server.Close()
+
+	tests := []struct {
+		name     string
+		method   string
+		params   map[string]interface{}
+		response interface{}
+	}{
+		{"Health", "health", map[string]interface{}{}, new(ctypes.ResultHealth)},
+		{"Status", "status", map[string]interface{}{}, new(ctypes.ResultStatus)},
+		{"NetInfo", "net_info", map[string]interface{}{}, new(ctypes.ResultNetInfo)},
+		{"Blockchain", "blockchain", map[string]interface{}{}, new(ctypes.ResultBlockchainInfo)},
+		{"Genesis", "genesis", map[string]interface{}{}, new(ctypes.ResultGenesis)},
+		{"GenesisChunk", "genesis_chunked", map[string]interface{}{}, new(ctypes.ResultGenesisChunk)},
+		{"Block", "block", map[string]interface{}{}, new(ctypes.ResultBlock)},
+		{"BlockResults", "block_results", map[string]interface{}{}, new(ctypes.ResultBlockResults)},
+		{"Commit", "commit", map[string]interface{}{}, new(ctypes.ResultCommit)},
+		{"Header", "header", map[string]interface{}{}, new(ctypes.ResultHeader)},
+		{"HeaderByHash", "header_by_hash", map[string]interface{}{}, new(ctypes.ResultHeader)},
+		{"CheckTx", "check_tx", map[string]interface{}{}, new(ctypes.ResultCheckTx)},
+		{"Tx", "tx", map[string]interface{}{}, new(ctypes.ResultTx)},
+		{"TxSearch", "tx_search", map[string]interface{}{}, new(ctypes.ResultTxSearch)},
+		{"BlockSearch", "block_search", map[string]interface{}{}, new(ctypes.ResultBlockSearch)},
+		{"Validators", "validators", map[string]interface{}{}, new(ctypes.ResultValidators)},
+		{"DumpConsensusState", "dump_consensus_state", map[string]interface{}{}, new(ctypes.ResultDumpConsensusState)},
+		{"ConsensusState", "consensus_state", map[string]interface{}{}, new(ctypes.ResultConsensusState)},
+		{"ConsensusParams", "consensus_params", map[string]interface{}{}, new(ctypes.ResultConsensusParams)},
+		{"UnconfirmedTxs", "unconfirmed_txs", map[string]interface{}{}, new(ctypes.ResultUnconfirmedTxs)},
+		{"NumUnconfirmedTxs", "num_unconfirmed_txs", map[string]interface{}{}, new(ctypes.ResultUnconfirmedTxs)},
+		{"BroadcastTxSync", "broadcast_tx_sync", map[string]interface{}{}, new(ctypes.ResultBroadcastTx)},
+		{"BroadcastTxAsync", "broadcast_tx_async", map[string]interface{}{}, new(ctypes.ResultBroadcastTx)},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_, err := client.Call(context.Background(), tt.method, tt.params, tt.response)
+			require.NoError(t, err)
+			t.Logf("%s result %+v", tt.name, tt.response)
+		})
+	}
 }
