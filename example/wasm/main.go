2b14c78bfc94933da21c14178cceff0f24b5eb49
ramil <ramilexe@gmail.com>
2024-05-14 11:54:10 +0300

    pass AppOptions param to NewWasmApp

--- example/wasm/main.go
+++ example/wasm/main.go
@@ -3,11 +3,14 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
 
 	"cosmossdk.io/log"
 	"github.com/CosmWasm/wasmd/app"
+	"github.com/CosmWasm/wasmd/x/wasm/keeper"
 	dbm "github.com/cosmos/cosmos-db"
 	"github.com/cosmos/cosmos-sdk/server"
+	"github.com/cosmos/cosmos-sdk/testutil/sims"
 
 	"github.com/consideritdone/landslidevm"
 )
@@ -18,7 +21,7 @@ func main() {
 		panic(err)
 	}
 	logger := log.NewNopLogger()
-	wasmApp := app.NewWasmApp(logger, db, nil, true, nil, nil)
+	wasmApp := app.NewWasmApp(logger, db, nil, true, sims.NewAppOptionsWithFlagHome(os.TempDir()), []keeper.Option{})
 
 	appCreator := landslidevm.NewLocalAppCreator(server.NewCometABCIWrapper(wasmApp))
 	if err := landslidevm.Serve(context.Background(), appCreator); err != nil {
