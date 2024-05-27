8f18c714c451326bcf9f46146feec66de2804d4f
Ivan Sukach <ivansukach@github.com>
2024-05-27 19:31:35 +0200

    finalize network service unit test

--- vm/rpc_test.go
+++ vm/rpc_test.go
@@ -2,9 +2,11 @@ package vm
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"github.com/cometbft/cometbft/abci/example/kvstore"
 	"github.com/cometbft/cometbft/libs/rand"
+	"github.com/cometbft/cometbft/types"
 	"github.com/cometbft/cometbft/version"
 	vmpb "github.com/consideritdone/landslidevm/proto/vm"
 	"net/http"
@@ -130,6 +132,67 @@ func testBroadcastTxAsync(t *testing.T, client *client.Client, vm *LandslideVM, 
 	return result
 }
 
+func testStatus(t *testing.T, client *client.Client, expected *coretypes.ResultStatus) {
+	result := new(coretypes.ResultStatus)
+	_, err := client.Call(context.Background(), "status", map[string]interface{}{}, result)
+	require.NoError(t, err)
+	require.Equal(t, expected.NodeInfo.Moniker, result.NodeInfo.Moniker)
+}
+
+func testNetInfo(t *testing.T, client *client.Client, expected *coretypes.ResultNetInfo) {
+	result := new(coretypes.ResultNetInfo)
+	_, err := client.Call(context.Background(), "net_info", map[string]interface{}{}, result)
+	require.NoError(t, err)
+	//TODO: check equality
+	//require.Equal(t, expected.Listening, result.Listening)
+	//require.Equal(t, expected.Peers, result.Peers)
+	//require.Equal(t, expected.Listeners, result.Listeners)
+	//require.Equal(t, expected.NPeers, result.NPeers)
+	//TODO: OR compare to desired values
+	//require.NoError(t, err, "%d: %+v", i, err)
+	//assert.True(t, netinfo.Listening)
+	//assert.Empty(t, netinfo.Peers)
+
+}
+
+func testConsensusState(t *testing.T, client *client.Client, expected *coretypes.ResultConsensusState) {
+	result := new(coretypes.ResultConsensusState)
+	_, err := client.Call(context.Background(), "consensus_state", map[string]interface{}{}, result)
+	require.NoError(t, err)
+	//TODO: check equality
+	//require.Equal(t, expected.RoundState, result.RoundState)
+	//TODO: OR compare to desired values
+	//assert.NotEmpty(t, cons.RoundState)
+}
+
+func testDumpConsensusState(t *testing.T, client *client.Client, expected *coretypes.ResultDumpConsensusState) {
+	result := new(coretypes.ResultDumpConsensusState)
+	_, err := client.Call(context.Background(), "dump_consensus_state", map[string]interface{}{}, result)
+	require.NoError(t, err)
+	//TODO: check equality
+	//require.Equal(t, expected.RoundState, result.RoundState)
+	//require.EqualValues(t, expected.Peers, result.Peers)
+	//TODO: OR compare to desired values
+	//assert.NotEmpty(t, cons.RoundState)
+	//require.ElementsMatch(t, expected.Peers, result.Peers)
+}
+
+func testConsensusParams(t *testing.T, client *client.Client, params map[string]interface{}, expected *coretypes.ResultConsensusParams) {
+	result := new(coretypes.ResultConsensusParams)
+	_, err := client.Call(context.Background(), "consensus_params", params, result)
+	require.NoError(t, err)
+	//TODO: check equality
+	require.Equal(t, expected.BlockHeight, result.BlockHeight)
+	//require.Equal(t, expected.ConsensusParams.Version.App, result.ConsensusParams.Version.App)
+	//require.Equal(t, expected.ConsensusParams.Hash(), result.ConsensusParams.Hash())
+}
+
+func testHealth(t *testing.T, client *client.Client) {
+	result := new(coretypes.ResultHealth)
+	_, err := client.Call(context.Background(), "health", map[string]interface{}{}, result)
+	require.NoError(t, err)
+}
+
 func checkTxResult(t *testing.T, client *client.Client, vm *LandslideVM, env *txRuntimeEnv) {
 	ctx, cancelCtx := context.WithTimeout(context.Background(), 10*time.Second)
 	for {
@@ -216,6 +279,13 @@ func TestABCIService(t *testing.T) {
 			result := testBroadcastTxSync(t, client, vm, map[string]interface{}{"tx": tx})
 			checkTxResult(t, client, vm, &txRuntimeEnv{key: k, value: v, hash: result.Hash, initHeight: initHeight})
 		}
+		cancel()
+		_, _, tx := MakeTxKV()
+		initMempoolSize := vm.mempool.Size()
+		result := testBroadcastTxSync(t, client, vm, map[string]interface{}{"tx": tx})
+		require.Equal(t, initMempoolSize+1, vm.mempool.Size())
+		require.EqualValues(t, string(tx), result.Data.String())
+		require.EqualValues(t, types.Tx(tx), vm.mempool.ReapMaxTxs(-1)[0])
 	})
 }
 
@@ -227,6 +297,79 @@ func TestStatusService(t *testing.T) {
 
 	t.Run("Status", func(t *testing.T) {
 		testStatus(t, client, &coretypes.ResultStatus{})
+	})
+}
+
+//func TestStatusService(t *testing.T) {
+//	vm, service, _ := mustNewCounterTestVm(t)
+//
+//	blk0, err := vm.BuildBlock(context.Background())
+//	assert.ErrorIs(t, err, errNoPendingTxs, "expecting error no txs")
+//	assert.Nil(t, blk0)
+//
+//	txReply, err := service.BroadcastTxSync(&rpctypes.Context{}, []byte{0x01})
+//	assert.NoError(t, err)
+//	assert.Equal(t, atypes.CodeTypeOK, txReply.Code)
+//
+//	t.Run("Status", func(t *testing.T) {
+//		reply1, err := service.Status(&rpctypes.Context{})
+//		assert.NoError(t, err)
+//		assert.Equal(t, int64(1), reply1.SyncInfo.LatestBlockHeight)
+//
+//		blk, err := vm.BuildBlock(context.Background())
+//		assert.NoError(t, err)
+//		assert.NotNil(t, blk)
+//		assert.NoError(t, blk.Accept(context.Background()))
+//
+//		reply2, err := service.Status(&rpctypes.Context{})
+//		assert.NoError(t, err)
+//		assert.Equal(t, int64(2), reply2.SyncInfo.LatestBlockHeight)
+//	})
+//}
+
+func TestNetworkService(t *testing.T) {
+	server, vm, client, cancel := setupRPC(t, buildAccept)
+	defer server.Close()
+	defer cancel()
+
+	t.Run("NetInfo", func(t *testing.T) {
+		testNetInfo(t, client, &coretypes.ResultNetInfo{
+			Listening: true,
+			Listeners: nil,
+			NPeers:    0,
+			Peers:     nil,
+		})
+	})
+
+	t.Run("DumpConsensusState", func(t *testing.T) {
+		testDumpConsensusState(t, client, &coretypes.ResultDumpConsensusState{
+			RoundState: json.RawMessage{},
+			Peers:      []coretypes.PeerStateInfo{},
+		})
+	})
+
+	t.Run("ConsensusState", func(t *testing.T) {
+		testConsensusState(t, client, &coretypes.ResultConsensusState{
+			RoundState: json.RawMessage{},
+		})
+	})
+
+	t.Run("ConsensusParams", func(t *testing.T) {
+		initialHeight := vm.state.LastBlockHeight
+		for i := 0; i < 3; i++ {
+			_, _, tx := MakeTxKV()
+			result := testBroadcastTxCommit(t, client, vm, map[string]interface{}{"tx": tx})
+			require.EqualValues(t, result.Height, initialHeight+int64(1)+int64(i))
+			testConsensusParams(t, client, map[string]interface{}{"height": vm.state.LastBlockHeight}, &coretypes.ResultConsensusParams{
+				BlockHeight: result.Height,
+				//TODO: compare consensus params
+				//ConsensusParams: types.ConsensusParams{},
+			})
+		}
+	})
+
+	t.Run("Health", func(t *testing.T) {
+		testHealth(t, client)
 	})
 }
 
@@ -242,13 +385,6 @@ func TestHealth(t *testing.T) {
 	t.Logf("Health result %+v", result)
 }
 
-func testStatus(t *testing.T, client *client.Client, expected *coretypes.ResultStatus) {
-	result := new(coretypes.ResultStatus)
-	_, err := client.Call(context.Background(), "status", map[string]interface{}{}, result)
-	require.NoError(t, err)
-	require.Equal(t, expected.NodeInfo.Moniker, result.NodeInfo.Moniker)
-}
-
 func TestRPC(t *testing.T) {
 	//TODO: complicated combinations
 	server, _, client, cancel := setupRPC(t, buildAccept)
@@ -261,9 +397,9 @@ func TestRPC(t *testing.T) {
 		params   map[string]interface{}
 		response interface{}
 	}{
-		//{"Health", "health", map[string]interface{}{}, new(ctypes.ResultHealth)},
-		//{"Status", "status", map[string]interface{}{}, new(ctypes.ResultStatus)},
-		//{"NetInfo", "net_info", map[string]interface{}{}, new(ctypes.ResultNetInfo)},
+		//+{"Health", "health", map[string]interface{}{}, new(ctypes.ResultHealth)},
+		//+{"Status", "status", map[string]interface{}{}, new(ctypes.ResultStatus)},
+		//?{"NetInfo", "net_info", map[string]interface{}{}, new(ctypes.ResultNetInfo)},
 		//{"Blockchain", "blockchain", map[string]interface{}{}, new(ctypes.ResultBlockchainInfo)},
 		//{"Genesis", "genesis", map[string]interface{}{}, new(ctypes.ResultGenesis)},
 		//{"GenesisChunk", "genesis_chunked", map[string]interface{}{}, new(ctypes.ResultGenesisChunk)},
@@ -277,9 +413,9 @@ func TestRPC(t *testing.T) {
 		//{"TxSearch", "tx_search", map[string]interface{}{}, new(ctypes.ResultTxSearch)},
 		//{"BlockSearch", "block_search", map[string]interface{}{}, new(ctypes.ResultBlockSearch)},
 		//{"Validators", "validators", map[string]interface{}{}, new(ctypes.ResultValidators)},
-		//{"DumpConsensusState", "dump_consensus_state", map[string]interface{}{}, new(ctypes.ResultDumpConsensusState)},
-		//{"ConsensusState", "consensus_state", map[string]interface{}{}, new(ctypes.ResultConsensusState)},
-		//{"ConsensusParams", "consensus_params", map[string]interface{}{}, new(ctypes.ResultConsensusParams)},
+		//?{"DumpConsensusState", "dump_consensus_state", map[string]interface{}{}, new(ctypes.ResultDumpConsensusState)},
+		//?{"ConsensusState", "consensus_state", map[string]interface{}{}, new(ctypes.ResultConsensusState)},
+		//?{"ConsensusParams", "consensus_params", map[string]interface{}{}, new(ctypes.ResultConsensusParams)},
 		//{"UnconfirmedTxs", "unconfirmed_txs", map[string]interface{}{}, new(ctypes.ResultUnconfirmedTxs)},
 		//{"NumUnconfirmedTxs", "num_unconfirmed_txs", map[string]interface{}{}, new(ctypes.ResultUnconfirmedTxs)},
 		//+{"BroadcastTxSync", "broadcast_tx_sync", map[string]interface{}{}, new(ctypes.ResultBroadcastTx)},
