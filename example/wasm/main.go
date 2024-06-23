1cf70ef08031e7f7a0bc8a86fbf60af2ef393a02
Ramil Amerzyanov <ramilexe@gmail.com>
2024-06-24 00:11:14 +0500

    add persistence storage for KVStore and Wasm app (#40)
    bump AvalancheGo version to v1.11.8 (used in e2e tests)

--- example/wasm/main.go
+++ example/wasm/main.go
@@ -16,25 +16,32 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
 	"github.com/consideritdone/landslidevm"
+	"github.com/consideritdone/landslidevm/vm"
 )
 
 func main() {
-	db, err := dbm.NewDB("dbName", dbm.MemDBBackend, "")
-	if err != nil {
-		panic(err)
-	}
-	logger := log.NewNopLogger()
-
-	cfg := sdk.GetConfig()
-	cfg.SetBech32PrefixForAccount(app.Bech32PrefixAccAddr, app.Bech32PrefixAccPub)
-	cfg.SetBech32PrefixForValidator(app.Bech32PrefixValAddr, app.Bech32PrefixValPub)
-	cfg.SetBech32PrefixForConsensusNode(app.Bech32PrefixConsAddr, app.Bech32PrefixConsPub)
-	cfg.SetAddressVerifier(wasmtypes.VerifyAddressLen())
-	cfg.Seal()
-	wasmApp := app.NewWasmApp(logger, db, nil, true, sims.NewAppOptionsWithFlagHome(os.TempDir()), []keeper.Option{}, baseapp.SetChainID("landslide-test"))
-
-	appCreator := landslidevm.NewLocalAppCreator(server.NewCometABCIWrapper(wasmApp))
+	appCreator := WasmCreator()
 	if err := landslidevm.Serve(context.Background(), appCreator); err != nil {
 		panic(fmt.Sprintf("can't serve application: %s", err))
+	}
+}
+
+func WasmCreator() vm.AppCreator {
+	return func(config *vm.AppCreatorOpts) (vm.Application, error) {
+		db, err := dbm.NewDB("wasm", dbm.GoLevelDBBackend, config.ChainDataDir)
+		if err != nil {
+			panic(err)
+		}
+		logger := log.NewNopLogger()
+
+		cfg := sdk.GetConfig()
+		cfg.SetBech32PrefixForAccount(app.Bech32PrefixAccAddr, app.Bech32PrefixAccPub)
+		cfg.SetBech32PrefixForValidator(app.Bech32PrefixValAddr, app.Bech32PrefixValPub)
+		cfg.SetBech32PrefixForConsensusNode(app.Bech32PrefixConsAddr, app.Bech32PrefixConsPub)
+		cfg.SetAddressVerifier(wasmtypes.VerifyAddressLen())
+		cfg.Seal()
+		wasmApp := app.NewWasmApp(logger, db, nil, true, sims.NewAppOptionsWithFlagHome(os.TempDir()), []keeper.Option{}, baseapp.SetChainID("landslide-test"))
+
+		return server.NewCometABCIWrapper(wasmApp), nil
 	}
 }
