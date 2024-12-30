b02960588ca3d187cefd8378fad79c88d4bbca08
Ivan Sukach <ivansukach@github.com>
2024-12-30 06:51:53 +0100

    setup exception comment for linter

--- utils/hashing/hashing.go
+++ utils/hashing/hashing.go
@@ -5,7 +5,7 @@ import (
 	"errors"
 	"fmt"
 
-	//lint:ignore SA1019,G507 it is necessary for compatibility with subnet-evm
+	//lint:ignore SA1019,gosec it is necessary for compatibility with subnet-evm
 	"golang.org/x/crypto/ripemd160"
 )
 
