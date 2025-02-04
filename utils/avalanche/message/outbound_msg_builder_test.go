c187b2ec9a7a7b5e0cf32054edaac8379808bf96
Ivan Sukach <ivansukach@github.com>
2025-02-04 17:51:56 +0100

    test: outbound msg builder

--- utils/avalanche/message/outbound_msg_builder_test.go
+++ utils/avalanche/message/outbound_msg_builder_test.go
@@ -3,44 +3,43 @@
 
 package message
 
-//
-//import (
-//	"testing"
-//	"time"
-//
-//	"github.com/prometheus/client_golang/prometheus"
-//	"github.com/stretchr/testify/require"
-//
-//	"github.com/ava-labs/avalanchego/ids"
-//	"github.com/ava-labs/avalanchego/utils/compression"
-//	"github.com/ava-labs/avalanchego/utils/logging"
-//)
-//
-//func Test_newOutboundBuilder(t *testing.T) {
-//	t.Parallel()
-//
-//	mb, err := newMsgBuilder(
-//		logging.NoLog{},
-//		prometheus.NewRegistry(),
-//		10*time.Second,
-//	)
-//	require.NoError(t, err)
-//
-//	for _, compressionType := range []compression.Type{
-//		compression.TypeNone,
-//		compression.TypeZstd,
-//	} {
-//		t.Run(compressionType.String(), func(t *testing.T) {
-//			builder := newOutboundBuilder(compressionType, mb)
-//
-//			outMsg, err := builder.GetAcceptedStateSummary(
-//				ids.GenerateTestID(),
-//				12345,
-//				time.Hour,
-//				[]uint64{1000, 2000},
-//			)
-//			require.NoError(t, err)
-//			t.Logf("outbound message with compression type %s built message with size %d", compressionType, len(outMsg.Bytes()))
-//		})
-//	}
-//}
+import (
+	"github.com/cometbft/cometbft/libs/log"
+	"testing"
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/stretchr/testify/require"
+
+	"github.com/landslidenetwork/slide-sdk/utils/avalanche/compression"
+	"github.com/landslidenetwork/slide-sdk/utils/ids"
+)
+
+func Test_newOutboundBuilder(t *testing.T) {
+	t.Parallel()
+
+	mb, err := newMsgBuilder(
+		log.NewNopLogger(),
+		prometheus.NewRegistry(),
+		10*time.Second,
+	)
+	require.NoError(t, err)
+
+	for _, compressionType := range []compression.Type{
+		compression.TypeNone,
+		compression.TypeZstd,
+	} {
+		t.Run(compressionType.String(), func(t *testing.T) {
+			builder := newOutboundBuilder(compressionType, mb)
+
+			outMsg, err := builder.AppRequest(
+				ids.GenerateTestID(),
+				12345,
+				time.Hour,
+				[]byte{1, 2, 3, 4, 5},
+			)
+			require.NoError(t, err)
+			t.Logf("outbound message with compression type %s built message with size %d", compressionType, len(outMsg.Bytes()))
+		})
+	}
+}
