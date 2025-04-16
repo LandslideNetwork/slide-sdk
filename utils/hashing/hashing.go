25d423518b4881c21fe324f07852039fb21bdb8f
Ivan Sukach <ivansukach@github.com>
2025-04-16 03:19:37 +0200

    remove linter exception for ripemd.New() call

--- utils/hashing/hashing.go
+++ utils/hashing/hashing.go
@@ -49,7 +49,7 @@ func ComputeHash160Array(buf []byte) Hash160 {
 // ComputeHash160 computes a cryptographically strong 160 bit hash of the input
 // byte slice.
 func ComputeHash160(buf []byte) []byte {
-	ripe := ripemd160.New() //lint:ignore SA1019 the library imported from avalanche
+	ripe := ripemd160.New()
 	_, err := io.Writer(ripe).Write(buf)
 	if err != nil {
 		panic(err)
