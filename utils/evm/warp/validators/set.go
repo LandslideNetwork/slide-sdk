1b8039e7230f13780c231611f810e34a9f80845f
Ivan Sukach <ivansukach@github.com>
2025-04-21 22:55:48 +0200

    remove errInsufficientWeight

--- utils/evm/warp/validators/set.go
+++ utils/evm/warp/validators/set.go
@@ -24,7 +24,6 @@ var (
 	errDuplicateValidator   = errors.New("duplicate validator")
 	errMissingValidator     = errors.New("missing validator")
 	errTotalWeightNotUint64 = errors.New("total weight is not a uint64")
-	errInsufficientWeight   = errors.New("insufficient weight")
 )
 
 // newSet returns a new, empty set of validators.
