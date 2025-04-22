84f6cd52cf9bf4343e4a94f836fefbf23c5578cc
Ivan Sukach <ivansukach@github.com>
2025-04-22 02:42:20 +0200

    remove newNodeIDConnector

--- utils/avalanche/network/test_network.go
+++ utils/avalanche/network/test_network.go
@@ -54,10 +54,6 @@ type nodeIDConnector struct {
 	nodeID ids.NodeID
 }
 
-func newNodeIDConnector(nodeID ids.NodeID) *nodeIDConnector {
-	return &nodeIDConnector{nodeID: nodeID}
-}
-
 func (f *nodeIDConnector) IsAllowed(nodeID ids.NodeID, _ bool) bool {
 	return nodeID == f.nodeID
 }
