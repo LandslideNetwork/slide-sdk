f913592d38984a1e6aeb88af526c59c59019f34a
Ivan Sukach <ivansukach@github.com>
2025-04-22 03:15:55 +0200

    add defaultPeerVersion variable

--- utils/evm/peer/network_test.go
+++ utils/evm/peer/network_test.go
@@ -2,3 +2,13 @@
 // See the file LICENSE for licensing terms.
 
 package peer
+
+import "github.com/landslidenetwork/slide-sdk/utils/version"
+
+var (
+	defaultPeerVersion = &version.Application{
+		Major: 1,
+		Minor: 0,
+		Patch: 0,
+	}
+)
