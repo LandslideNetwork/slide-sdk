8336f5a40ece078d92e97e0b660e563c853e3a2f
Ivan Sukach <ivansukach@github.com>
2025-05-19 23:34:35 +0300

    change ava-labs dependency to slide-sdk utility

--- utils/math/averager_heap_test.go
+++ utils/math/averager_heap_test.go
@@ -9,7 +9,7 @@ import (
 
 	"github.com/stretchr/testify/require"
 
-	"github.com/ava-labs/avalanchego/ids"
+	"github.com/landslidenetwork/slide-sdk/utils/ids"
 )
 
 func TestAveragerHeap(t *testing.T) {
